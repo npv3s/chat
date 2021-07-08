c9fc73a46822e474c72100c1a67bf818971f9c1b
Nikolai <npv3s@skopa.dev>
2021-07-08 14:49:25 +0300

    Перенесен js-скрипт

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'front/dist')));
 
 app.get('/', (req, res) => {
     res.sendFile('public/index.html');
